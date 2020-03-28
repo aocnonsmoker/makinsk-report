21959c2648959473e863b1f064d2f482e6728541
aocnonsmoker <aocnonsmoker@gmail.com>
2020-03-28 15:20:45 +0600

    php conn checker

--- config.php
+++ config.php
@@ -1,3 +1,14 @@
 <?php
-    echo "Connected"
+    $host = "srv-pleskdb23.ps.kz";
+	$dbUsername = "rahme_login";
+	$dbPassword = "nonsmoker123";
+    $dbname = "rahmetm1_login";
+    
+    $connection = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+    if (mysqli_connect_error()) {
+		die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+	} else {
+        echo "Connected"
+		$conn->close();
+	}
 ?>
